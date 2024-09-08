e33fffcc21363c2ff6ac5c9a08e0d49ac9e3c661
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-09-08 07:45:02 +0200

    added better images

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -7,9 +7,13 @@ type Props = {};
 function LandingPage({}: Props) {
   return (
     <div className="mx-auto flex max-w-screen-lg flex-col items-center gap-10 px-4 pt-20">
-      <h1 className="text-4xl font-bold tracking-tight md:text-6xl">
-        🐕 Dog Happiness Tracker
-      </h1>
+      <div className="flex flex-row">
+        <img src="/happy-dog.png" className="h-16 w-16" alt="" />
+        <h1 className="text-4xl font-bold tracking-tight md:text-6xl">
+          {" "}
+          Dog Happiness Tracker
+        </h1>
+      </div>
       <p className="mt-4 text-center text-lg text-gray-600 md:text-xl">
         Find out if your friend is getting enough exercise and attention to stay
         healthy and happy! 🐾{" "}
