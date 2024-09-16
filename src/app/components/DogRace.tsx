9d154735da5d1172280d959360a0bd8e41f7da3f
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-09-16 13:17:37 +0200

    modifying shadcn carusell to match my old card

--- src/app/components/DogRace.tsx
+++ src/app/components/DogRace.tsx
@@ -31,8 +31,35 @@ function DogRace({ susge, susge2 }: { susge: string; susge2: string }) {
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                  <div className="flex flex-col">
+                    <img
+                      src={DogRaces[currentIndex]?.img}
+                      alt="dograce"
+                      className="mb-4 h-48 w-full rounded-lg object-cover shadow-md"
+                    />
+                    <p className="mb-1 text-xl font-medium text-gray-800">
+                      {DogRaces[currentIndex]?.name} 🏆
+                    </p>
+                  </div>
                 </CardContent>
+                <div className="flex w-full flex-col space-y-2 px-4">
+                  <p className="text-sm font-semibold text-gray-600">
+                    ⚡ Activity Level
+                  </p>
+                  <progress
+                    className="progress progress-accent w-full"
+                    value={DogRaces[currentIndex]?.activity_level}
+                    max="100"
+                  ></progress>
+
+                  <p className="text-sm font-semibold text-gray-600">
+                    🏃‍♂️ Exercise Needs
+                  </p>
+                  <p className="text-sm text-gray-700">
+                    {DogRaces[currentIndex]?.exercise_needs ||
+                      "ℹ️ Information not available"}
+                  </p>
+                </div>
               </Card>
             </div>
           </CarouselItem>
