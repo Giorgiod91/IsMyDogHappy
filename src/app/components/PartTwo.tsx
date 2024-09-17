34d076eabc99abf3438faf181fbd810e89fd9138
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-09-17 14:12:01 +0200

    added framer motion effects to the content divs

--- src/app/components/PartTwo.tsx
+++ src/app/components/PartTwo.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { div } from "framer-motion/client";
+import { div, p } from "framer-motion/client";
 import Image from "next/image";
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function PartTwo({}: Props) {
   const [clicked, setClicked] = useState(false);
+  const [clickedIndex, setClickedIndex] = useState<number | null>(null);
+  // const [flipClicked, setFlipClicked] = useState(false);
   const trackHashMap = new Map();
-  trackHashMap.set({ name: "walking", emoji: "🐕" }, 2);
+  trackHashMap.set(
+    {
+      name: "walking",
+      emoji: "🐕",
+      description: "walk with your dog 30 to 100 min ",
+    },
+    2,
+  );
   trackHashMap.set({ name: "playing", emoji: "/play.png" }, 3);
 
   const sortedTrackHashMap = Array.from(trackHashMap).sort(
@@ -16,22 +26,55 @@ function PartTwo({}: Props) {
   );
 
   return (
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center space-y-10">
       <h1 className="text-pretty text-center text-7xl font-extrabold text-white">
         {" "}
         What to Track
       </h1>
       <div className="flex flex-col items-center justify-center gap-16 lg:flex-row">
-        {Array.from(sortedTrackHashMap).map(([key, value], index) => (
+        {Array.from(sortedTrackHashMap).map(([key], index) => (
           <div id={index.toString()} className="flex flex-col">
             {key.emoji.includes(".png") ? (
-              <div className="flex border-4">
+              <motion.div
+                whileHover={{
+                  backgroundColor: "red",
+                  scale: 1.1,
+                }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                className="border-1 flex h-[130px] w-[230px] cursor-pointer flex-col items-center justify-center shadow-lg"
+              >
                 <Image width={100} height={100} src={key.emoji} alt="" />
-              </div>
+                <p className="text-white">{key.name}</p>
+              </motion.div>
             ) : (
-              <div className="flex border-4">
-                <h1>{key.emoji}</h1>
-              </div>
+              /* emoji part here */
+              <motion.div
+                onClick={() =>
+                  setClickedIndex(clickedIndex === index ? null : index)
+                }
+                whileHover={{
+                  backgroundColor: "red",
+                  scale: 1.1,
+                }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                className="border-1 flex h-[130px] w-[230px] cursor-pointer flex-col items-center justify-center shadow-lg"
+              >
+                {" "}
+                {clickedIndex == index ? (
+                  <div
+                    onClick={() =>
+                      setClickedIndex(clickedIndex === index ? null : index)
+                    }
+                  >
+                    <p className="text-white">{key.description}</p>
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center justify-center">
+                    <span className="cursor-pointer">{key.emoji}</span>
+                    <h3 className="text-white">{key.name}</h3>
+                  </div>
+                )}
+              </motion.div>
             )}
           </div>
         ))}
