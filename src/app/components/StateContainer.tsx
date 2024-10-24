21bd95b9a0d39fb60511377cb5e6ef45c774f8cf
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-10-24 14:42:15 +0200

    made better styling oof the h2 to fit rest of the app

--- src/app/components/StateContainer.tsx
+++ src/app/components/StateContainer.tsx
@@ -12,7 +12,9 @@ function StateContainer({ susge, susge2 }: { susge: string; susge2: string }) {
       className="relative mx-auto mt-12 flex max-w-4xl flex-col items-center justify-center rounded-lg bg-gray-900 p-6 shadow-2xl"
     >
       <h2 className="mb-6 text-4xl font-extrabold text-white md:text-7xl">
-        Your Dog's Stats
+        <span className="text-[#10B981]">Y</span>
+        our <span className="text-[#10B981]">D</span>og's{" "}
+        <span className="text-[#10B981]">S</span>tats
       </h2>
       <div className="flex w-full flex-col gap-6 overflow-x-scroll md:grid md:flex-none md:grid-cols-2 md:overflow-visible">
         <div className="flex flex-col items-center justify-center rounded-lg p-6 shadow-md">
